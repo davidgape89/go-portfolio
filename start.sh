13412e6180aeb9a64cd942271dae98a22d824cbd
David Garcia Perez <david@localhost.localdomain>
2020-02-07 00:01:05 +0100

    Implemented watcher

--- start.sh
+++ start.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-go run main.go router.go db.go
+
+go run main.go &
+
+inotifywait -q -m -e close_write *.go **/*.go |
+while read -r file event; do
+    kill -9 $!
+    kill $(ps | grep main | awk '{print $1}')
+    echo "Recompiling..."
+    go run main.go &
+done
